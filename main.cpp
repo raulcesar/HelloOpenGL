3748d3221688a689d5afaec63f3a4abfd060be26
Raul Teixeira <raul.teixeira@camara.leg.br>
2015-04-22 11:23:08 -0300

    drawing triangle with shaders!

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
 #include <GL/glew.h>
 #define GLFW_DLL
@@ -9,7 +13,7 @@ using namespace std;
 
 void error_callback(int error, const char* description)
 {
-    fprintf(stderr, "Deu pau monstro n. %f: %s", error, description);
+	fprintf(stderr, "Deu pau monstro n. %f: %s", error, description);
 }
 
 // void window_close_callback(GLFWwindow* window)
@@ -19,20 +23,145 @@ void error_callback(int error, const char* description)
 //     //     glfwSetWindowShouldClose(window, GL_FALSE);
 // }
 
-void render(void)
-{
+// void render(GLint shaderProgram)
+// {
+// 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+// 	glUseProgram(shaderProgram);
 
-  glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
-  glClear (GL_COLOR_BUFFER_BIT);
+// 	glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
+// 	glClear (GL_COLOR_BUFFER_BIT);
 
-  glBegin (GL_TRIANGLES);
-  {
-    glColor3f (0.6, 0.3, 0.3);
-    glVertex2f (0, .5);
-    glVertex2f (-.5, -.5);
-    glVertex2f (.5, -.5);
-  }
-  glEnd ();
+// 	glBegin (GL_TRIANGLES);
+// 	{
+// 		glColor3f (0.6, 0.3, 0.3);
+// 		glVertex2f (0, .5);
+// 		glVertex2f (-.5, -.5);
+// 		glVertex2f (.5, -.5);
+// 	}
+// 	glEnd ();
+// }
+
+std::string get_shader_content(const char *filename) {
+	string shaderStr;
+	ifstream fileStream (filename, ios::in);
+	if (fileStream.is_open()) {
+		fileStream.seekg(0, ios::end);
+		shaderStr.resize(fileStream.tellg());
+		fileStream.seekg(0, ios::beg);
+		fileStream.read(&shaderStr[0], shaderStr.size());
+		fileStream.close();
+	}
+
+	return shaderStr;
+}
+
+string checkShaderStatus(GLint shader, GLenum status) {
+	GLint retStatus = 0;
+	string retStr;
+	glGetShaderiv(shader, status, &retStatus);
+	if (retStatus == GL_FALSE) {
+		GLint maxLength = 0;
+		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetShaderInfoLog(shader, maxLength, &maxLength, &errorLog[0]);
+
+		printf("Erro no shader:\n%s", &errorLog[0]);
+		printf("Terminou pau de shader\n");
+		// Provide the infolog in whatever manor you deem best.
+		retStr = string (&errorLog[0]);
+	}
+
+	return retStr;
+
+	// std::string str(ethernetCharArray);
+}
+string checkProgramStatus(GLint program, GLenum status) {
+	GLint retStatus = 0;
+	string retStr;
+	glGetProgramiv(program, status, &retStatus);
+	if (retStatus == GL_FALSE) {
+		GLint maxLength = 0;
+		glGetProgramiv(program, GL_INFO_LOG_LENGTH, &maxLength);
+
+		// The maxLength includes the NULL character
+		std::vector<GLchar> errorLog(maxLength);
+		glGetProgramInfoLog(program, maxLength, &maxLength, &errorLog[0]);
+
+		printf("Erro no program:\n%s", &errorLog[0]);
+		printf("Terminou pau de program\n");
+		// Provide the infolog in whatever manor you deem best.
+		retStr = string (&errorLog[0]);
+	}
+
+	return retStr;
+
+	// std::string str(ethernetCharArray);
+}
+
+
+GLint createShaderProgram() {
+	string vsString = get_shader_content("simple.vs");
+	string fsString = get_shader_content("simple.fs");
+	const char* vs = vsString.c_str();
+	const char* fs = fsString.c_str();
+
+	const char* separator = string(20, '*').c_str();
+	// printf("%s\n%s\n", string(20, '*').c_str());
+	printf("%s\nvertex shader:\n%s\n%s\n%s\nfragment shader:\n%s\n%s\n%s\n", separator, separator, vs, separator, separator, fs, separator);
+
+
+
+
+	// printf("fragment shader:\n%s", fs);
+
+	string shaderStatus;
+	//Create vertex shader
+	GLint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vs, NULL);
+	glCompileShader(vertexShader);
+
+
+	shaderStatus = checkShaderStatus(vertexShader, GL_COMPILE_STATUS);
+	if (!shaderStatus.empty()) {
+		// Exit with failure.
+		glDeleteShader(vertexShader); // Don't leak the shader.
+		printf("Erro no vertex shader:\n%s", shaderStatus.c_str());
+		return 0;
+	}
+
+	//Create fragment shader
+	GLint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fs, NULL);
+	glCompileShader(fragmentShader);
+
+	shaderStatus = checkShaderStatus(fragmentShader, GL_COMPILE_STATUS);
+	if (!shaderStatus.empty()) {
+		// Exit with failure.
+		glDeleteShader(vertexShader); // Don't leak the shader.
+		glDeleteShader(fragmentShader); // Don't leak the shader.
+		printf("Erro no fragment shader:\n%s", shaderStatus.c_str());
+		return 0;
+	}
+
+	//Create shader program.
+	GLint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+
+	shaderStatus = checkProgramStatus(shaderProgram, GL_LINK_STATUS);
+	if (!shaderStatus.empty()) {
+		// Exit with failure.
+		glDeleteShader(vertexShader); // Don't leak the shader.
+		glDeleteShader(fragmentShader); // Don't leak the shader.
+		glDeleteProgram(shaderProgram); // Don't leak the shader.
+		printf("Error when linking program:\n%s", shaderStatus.c_str());
+		return 0;
+	}
+
+	return shaderProgram;
 }
 
 int main() {
@@ -46,8 +175,8 @@ int main() {
 	printf("Setei..\n");
 // glfwWindowHint(GLFW_DEPTH_BITS, 16);
 //Probably going to need this on mac.
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -55,7 +184,7 @@ int main() {
 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 	// glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    GLFWwindow* window = glfwCreateWindow( 900, 600, "Gears", NULL, NULL );
+	GLFWwindow* window = glfwCreateWindow( 900, 600, "Gears", NULL, NULL );
 
 	// //Try to open a window...
 	if (!window) {
@@ -76,46 +205,61 @@ int main() {
 	const GLubyte* version = glGetString(GL_VERSION);
 	const GLubyte* glslversion = glGetString(GL_SHADING_LANGUAGE_VERSION);
 	printf("Renderer: %s. Version: %s. GLSL version: %s\n", renderer, version, glslversion);
-	GLfloat point[] = {
+	GLfloat points[] = {
 		0.0f, 0.5f, 0.0f,
-		0.5f, 0.5f, 0.0f,
-		0.5f, 0.5f, 0.0f
+		0.5f, -0.5f, 0.0f,
+		-0.5f, -0.5f, 0.0f
 	};
+	GLuint vbo = 0;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
-	// GLuint vbo = 0;
-	// glGenBuffers(1, &vbo);
-	// glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	// glBufferData(GL_ARRAY_BUFFER, sizeof(points), points, GL_STATIC_DRAW);
 
-    while( !glfwWindowShouldClose(window) )
-    {
+	GLuint vao = 0;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
 
-    	render();
+	GLint shaderProgram = createShaderProgram();
+	printf("ShaderProgram: %u\n", shaderProgram);
+	while ( !glfwWindowShouldClose(window) )
+	{
 
-    	glfwSwapBuffers(window);
-    	sleep(1);
-    	
-        // Draw gears
-        // draw();
+		// render(shaderProgram);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
 
-        // Update animation
-        // animate();
+		//draw 3 points
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        // Swap buffers
-        // glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
+		glfwPollEvents();
+		glfwSwapBuffers(window);
+		// sleep(1);
 
-    // Terminate GLFW
-    glfwDestroyWindow(window);
-    glfwTerminate();
+		// Draw gears
+		// draw();
 
-    // Exit program
-    
+		// Update animation
+		// animate();
+
+		// Swap buffers
+		// glfwSwapBuffers(window);
+	}
+
+	// Terminate GLFW
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
+	// Exit program
+
 
 
 
 
 	// printf("OpenGL %s, GLSL %s\n", glGetString(GL_VERSION), glGetString(GL_SHADING_LANGUAGE_VERSION));
-    return 0;
+	return 0;
 }
